62db3d0fee65a650e7a3007ff29fdc40045bd09f
Tayyab <tayyabali50055@gmail.com>
2025-07-07 12:37:34 +0500

    update cors

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors";
+
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
 
+
 dotenv.config();
 
 const app = express();
+
+app.use(cors({
+  origin: ['https://mernestate610.netlify.app', 'http://localhost:5173'],
+  credentials: true
+}));
 
 app.use(express.json());
 app.use(cookieParser());
